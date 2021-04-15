827cb0b5fd2ded59e851063abc4609c4eb39438f
Jenu Varghese Chacko <JenuC@users.noreply.github.com>
2021-04-15 10:50:12 -0500

    matching the timebase value from notebook #SP1

--- FLIM_DataProcessing/BP1_ImageJ_FLIM_Data_Processing/protocol_1.groovy
+++ FLIM_DataProcessing/BP1_ImageJ_FLIM_Data_Processing/protocol_1.groovy
@@ -8,7 +8,7 @@ import net.imglib2.img.Img
 import flimlib.flimj.FitParams
 fittingParameters = new FitParams()
 
-fittingParameters.xInc = 0.041 // nanoseconds
+fittingParameters.xInc = 0.039 // nanoseconds
 fittingParameters.ltAxis = 2 // XY<T> stack
 
 fittingParameters.transMap = img
